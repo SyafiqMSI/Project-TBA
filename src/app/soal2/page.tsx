64fa627afb4cd4a6dea2505bf4bc74403485808b
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-04-28 01:51:36 +0700

    faqih buatan abdan

--- src/app/soal2/page.tsx
+++ src/app/soal2/page.tsx
@@ -16,57 +16,11 @@ interface Transitions {
 }
 
 export default function Soal1() {
-    const [states, setStates] = useState<string>("q0,q1,q2");
-    const [alphabets, setAlphabets] = useState<string>("0,1");
-    const [startState, setStartState] = useState<string>("q0");
-    const [final, setFinalState] = useState<string>("q2");
-    const [jenisFA, setjenisFA] = useState<string>("nfa");
-    const [transitions, setTransitions] = useState<Transitions>({
-        q0: "q0,q1:q0",
-        q1: ":q2",
-        q2: ":",
-    });
-    const [epsilons, setEpsilons] = useState<{ [key: string]: string; }>({
-        q0: "q1",
-        q1: "q3",
-        q2: "q4",
-        q3: "",
-        q4: "",
-    });
+    const [regex, setStates] = useState<string>("0+1*");
 
     const handleStateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newStates = event.target.value;
         setStates(newStates);
-        const stateArray = newStates.split(',');
-        const newTransitions: Transitions = {};
-        stateArray.forEach(state => {
-            newTransitions[state] = transitions[state] || '';
-        });
-        setTransitions(newTransitions);
-    };
-
-    const handleAlphabetsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAlphabets(event.target.value);
-    };
-
-    const handleStartChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setStartState(event.target.value);
-    };
-
-    const handleFinalChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFinalState(event.target.value);
-    };
-
-    const handleAutomataTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setjenisFA(event.target.value);
-    };
-
-    const handleTransitionChange = (state: string, event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        setTransitions({
-            ...transitions,
-            [state]: value,
-        });
     };
 
     const onClickButtonGenerate = () => {
@@ -92,56 +46,17 @@ export default function Soal1() {
                 </div>
 
                 <div className="mt-5 space-y-2">
-                    <Label htmlFor="states" className="states">States</Label>
+                    <Label htmlFor="regex" className="regex">REGEX</Label>
                     <Input
                         type="text"
-                        placeholder="q0,q1,..."
-                        defaultValue={states}
+                        placeholder="1*"
+                        defaultValue={regex}
                         onChange={handleStateChange}
                     />
                 </div>
-                <div className="space-y-2">
-                    <Label htmlFor="alphabets" className="alphabets">Alphabets</Label>
-                    <Input
-                        type="text"
-                        placeholder="0,1,..."
-                        defaultValue={alphabets}
-                        onChange={handleAlphabetsChange}
-                    />
-                </div>
-                <div className="space-y-2">
-                    <Label htmlFor="startState" className="startState" >Start State</Label>
-                    <Input
-                        type="text"
-                        placeholder="q0/q1/..."
-                        value={startState}
-                        onChange={handleStartChange}
-                    />
-                </div>
-                <div className="space-y-2">
-                    <Label htmlFor="finalStates" >Final States</Label>
-                    <Input
-                        type="text"
-                        placeholder="q0/q1/..."
-                        value={final}
-                        onChange={handleFinalChange}
-                    />
-                </div>
-                <div className="mt-4 space-y-2 ">
-                    {Object.entries(transitions).map(([state, value], index) => (
-                        <div key={index} className="space-y-2">
-                            <Label htmlFor={state} className="Transitions" >Transitions {state} untuk {alphabets}</Label>
-                            <Input
-                                type="text"
-                                name={state}
-                                value={value}
-                                onChange={(e) => handleTransitionChange(state, e)}
-                            />
-                        </div>
-                    ))}
-                </div>
-
-
+                <div className="mt-8 px-1 py-5">
+                <Button onClick={onClickButtonGenerate}>Convert to e-NFA</Button>
+              </div>
             </div>
         </main>
     );
