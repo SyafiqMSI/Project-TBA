cb7a5355084b37c25c4c63560a25a30a61fb56f5
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-04-28 15:17:23 +0700

    feat: soal 4

--- src/app/soal4/ekuivalensi.ts
+++ src/app/soal4/ekuivalensi.ts
@@ -1,67 +1,36 @@
-interface Transitions {
-    [key: string]: string;
-}
-
 export interface DFA {
     states: string[];
     alphabets: string[];
     startState: string;
     finalStates: string[];
-    transitions: Transitions;
+    transitions: { [key: string]: string };
 }
 
-export function areEquivalent(dfa1: DFA, dfa2: DFA): boolean {
-    if (
-        dfa1.states.length !== dfa2.states.length ||
-        dfa1.finalStates.length !== dfa2.finalStates.length
-    ) {
-        return false;
-    }
 
-    // Sort final states to compare them
-    const sortedFinalStates1 = dfa1.finalStates.sort().join(',');
-    const sortedFinalStates2 = dfa2.finalStates.sort().join(',');
-    if (sortedFinalStates1 !== sortedFinalStates2) {
-        return false;
-    }
+function areEquivalent(dfa1: DFA, dfa2: DFA): boolean {
+    const visited: { [key: string]: boolean } = {};
+    const queue: { state1: string; state2: string }[] = [{ state1: dfa1.startState, state2: dfa2.startState }];
 
-    const distinguishTable: { [key: string]: { [key: string]: boolean } } = {};
-    for (const state1 of dfa1.states) {
-        distinguishTable[state1] = {};
-        for (const state2 of dfa2.states) {
-            // Initialize distinguishTable
-            distinguishTable[state1][state2] = !(dfa1.finalStates.includes(state1) === dfa2.finalStates.includes(state2));
-        }
-    }
+    while (queue.length > 0) {
+        const { state1, state2 } = queue.shift()!;
+        const key = `${state1},${state2}`;
+        if (visited[key]) continue;
+        visited[key] = true;
 
-    let changed = true;
-    while (changed) {
-        changed = false;
-        for (let i = 0; i < dfa1.states.length; i++) {
-            for (let j = 0; j < dfa2.states.length; j++) { // Fixing the loop
-                for (const symbol of dfa1.alphabets) {
-                    const nextState1 = dfa1.transitions[dfa1.states[i] + ':' + symbol];
-                    const nextState2 = dfa2.transitions[dfa2.states[j] + ':' + symbol]; // Fixing the nextState2
-                    if (distinguishTable[nextState1][nextState2]) {
-                        if (!distinguishTable[dfa1.states[i]][dfa2.states[j]]) {
-                            distinguishTable[dfa1.states[i]][dfa2.states[j]] = true;
-                            changed = true;
-                        }
-                    }
+        const isFinal1 = dfa1.finalStates.includes(state1);
+        const isFinal2 = dfa2.finalStates.includes(state2);
+
+        if (isFinal1 !== isFinal2) return false;
+
+        for (const alphabet of dfa1.alphabets) {
+            const nextState1 = dfa1.transitions[state1] && dfa1.transitions[state1].split(':')[dfa1.alphabets.indexOf(alphabet)];
+            const nextState2 = dfa2.transitions[state2] && dfa2.transitions[state2].split(':')[dfa2.alphabets.indexOf(alphabet)];
+            if (nextState1 && nextState2) {
+                const nextKey = `${nextState1},${nextState2}`;
+                if (!visited[nextKey]) {
+                    queue.push({ state1: nextState1, state2: nextState2 });
                 }
-            }
-        }
-    }
-
-    const startState1 = dfa1.startState;
-    const startState2 = dfa2.startState;
-    if (distinguishTable[startState1][startState2]) {
-        return false;
-    }
-
-    for (let i = 0; i < dfa1.states.length; i++) {
-        for (let j = 0; j < dfa2.states.length; j++) {
-            if (!distinguishTable[dfa1.states[i]][dfa2.states[j]]) { // Fixing the loop
+            } else if ((nextState1 && !nextState2) || (!nextState1 && nextState2)) {
                 return false;
             }
         }
@@ -69,3 +38,5 @@ export function areEquivalent(dfa1: DFA, dfa2: DFA): boolean {
 
     return true;
 }
+
+export { areEquivalent };
